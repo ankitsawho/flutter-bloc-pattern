1fcdac32aaa63e7e59c229da4031be4197999c51
Ankit Sahu <alpha@macbook.local>
2022-07-04 19:49:51 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_pattern_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BLoC App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: HomeScreen(),
+    );
+  }
+}
